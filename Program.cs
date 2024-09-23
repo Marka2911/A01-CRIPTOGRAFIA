58361e606d125aed304cc514d9462655fb3082c4
Marka291104 <thesword867@gmail.com>
2024-09-23 10:27:31 +0200

    Començament

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace A01_CRIPTOGRAFIA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
